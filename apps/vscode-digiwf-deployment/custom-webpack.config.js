749907105062bb79d06e050b8d155c93ce568edf
Lukas Mösle <lukas95m@yahoo.de>
2022-07-28 19:49:39 +0200

    provide custom webpack config for vscode extensions

---
+++ apps/vscode-digiwf-deployment/custom-webpack.config.js
@@ -0,0 +1,12 @@
+// Helper for combining webpack config objects
+const { merge } = require('webpack-merge');
+
+module.exports = (config, context) => {
+    return merge(config, {
+        // overwrite values here
+        externals: {
+            vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+            // modules added here also need to be added in the .vscodeignore file
+        },
+    });
+};
