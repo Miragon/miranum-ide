7dd3dc941d774cedbd5fa5979d1e2c12b2dbb201
jakobmertl <jakob.mertl@miragon.io>
2022-09-22 09:37:28 +0200

    feat/getToKnow - wrote a test for digiwf-deployment-plugin-rest, method deploy, negative case and read through the code

---
+++ libs/digiwf-deployment-plugin-rest/src/lib/digiwf-deployment-plugin-rest.spec.ts
@@ -0,0 +1,17 @@
+import {DigiwfDeploymentPluginRest} from "@miragon-process-ide/digiwf-deployment-plugin-rest";
+
+describe("deploy", () => {
+    it("should come back as false", async () => {
+        const ddpr = new DigiwfDeploymentPluginRest("test", [{name:"testfail",url:"www.xD.org"}]);
+        const artifact = {type: "type1",
+                        project: "project1",
+                        path: "path1",
+                        file: {name: "name", extension: "extension", content: "content", size: 1}
+                        };
+
+        const deploymentSuccess = await ddpr.deploy("hi", artifact);
+
+        expect(deploymentSuccess.success).toBeFalsy();
+        expect(deploymentSuccess.message).toEqual(`No target configured for hi`);
+    });
+});
