a2942c425a3654c6a0550fa5e11bc284502dff57
jakobmertl <jakob.mertl@miragon.io>
2022-11-16 08:18:37 +0100

    feat/VSExtension: Auslagern der DocHTML

--- apps/vscode-digiwf-deployment/src/main.ts
+++ apps/vscode-digiwf-deployment/src/main.ts
@@ -2,6 +2,7 @@ import * as vscode from "vscode";
 import { DigiwfLib} from "@miragon-process-ide/digiwf-lib";
 import { generate } from "./app/generate/generate";
 import { deployArtifact, getUriAndDeploy, mapProcessConfigToDigiwfLib } from "./app/deployment/deployment";
+import { getGenerateFileWebview, getGenerateProjectWebview } from "./webviews/inputs/generateInput";
 
 let digiwfLib = new DigiwfLib();
 
@@ -33,7 +34,7 @@ export async function activate(context: vscode.ExtensionContext) {
 
     const generateFile = vscode.commands.registerCommand("process-ide.generateFile", async () => {
         const panel = vscode.window.createWebviewPanel(
-            'catCoding',
+            'generate',
             'Generate',
             vscode.ViewColumn.One,
             {
@@ -54,7 +55,7 @@ export async function activate(context: vscode.ExtensionContext) {
 
     const generateProject = vscode.commands.registerCommand("process-ide.generateProject", async () => {
         const panel = vscode.window.createWebviewPanel(
-            'catCoding',
+            'generateProject',
             'Generate Project',
             vscode.ViewColumn.One,
             {
@@ -80,96 +81,8 @@ export async function activate(context: vscode.ExtensionContext) {
                             generateFile, generateProject);
 }
 
-// eslint-disable-next-line
-export function deactivate() { }
-
 //     -----------------------------HELPERS-----------------------------     \\
 //evtl. später in deploy auslagern?
 export function getDigiWfLib(): DigiwfLib {
     return digiwfLib;
-}
-
-//     ----------------------------Web-Views----------------------------     \\
-function getGenerateFileWebview() {
-    return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Generate</title>
-    </head>
-    <body>
-        <div>
-            <h3>name:</h3>
-            <textarea id="name">ProjectName</textarea>
-        </div>
-        <div>
-            <h3>type:</h3>
-            <textarea id="type">bpmn</textarea>
-        </div>
-        <div>
-            <h3>Path:</h3>
-            <textarea id="path">Users/jakobmertl/Desktop</textarea>
-        </div>
-        <button id="confirm">generate</button>
-        <script>
-            const vscode = acquireVsCodeApi();
-            let name = "test Project";
-            let type = "bpmn";
-            let path = "Users/jakobmertl/Desktop";
-
-            confirm = document.getElementById("confirm");
-            confirm.addEventListener("click", async () => {
-                name = document.getElementById("name").value;
-                type = document.getElementById("type").value;
-                path = document.getElementById("path").value;
-                if(name && type && path) {
-                    vscode.postMessage({
-                        message:'generate', name: name, type: type, path: path
-                    })
-                }
-            });
-
-        </script>
-    </body>
-    </html>`;
-}
-
-function getGenerateProjectWebview() {
-    return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Generate a Project</title>
-    </head>
-    <body>
-        <div>
-            <h3>Project name:</h3>
-            <textarea id="name">ProjectName</textarea>
-        </div>
-        <div>
-            <h3>Path:</h3>
-            <textarea id="path">Users/jakobmertl/Desktop/ProjectName</textarea>
-        </div>
-        <button id="confirm">generate</button>
-        <script>
-            const vscode = acquireVsCodeApi();
-            let name = "test Project";
-            let path = "Users/jakobmertl/Desktop";
-
-            confirm = document.getElementById("confirm");
-            confirm.addEventListener("click", async () => {
-                name = document.getElementById("name").value;
-                path = document.getElementById("path").value;
-                if(name && path) {
-                    vscode.postMessage({
-                        message:'generateProject', name: name, path: path
-                    })
-                }
-            });
-
-        </script>
-    </body>
-    </html>`;
 }
