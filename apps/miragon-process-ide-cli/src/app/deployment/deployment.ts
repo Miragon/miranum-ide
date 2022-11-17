06b469acfaf83f2722db9851a8cda0501d1381cf
Jakob Mertl <42836653+J-Mertl@users.noreply.github.com>
2022-11-17 09:12:17 +0100

    fix/smallBugs (#25)
    * fix/smallBugs: fixed generation of README.md
    
    * fix/smallBugs: added whitelist to deployment
    
    * fix/smallBugs: fixed unique name
    
    * fix/smallBugs: CLI-deployment fix
    
    * fix/smallBugs: fixed Tests
    
    * fix/smallBugs: implemented review
    
    * fix/smallBugs: fixed tests
    
    * fix/smallBugs: implemented feedback
    
    * fix/smallBugs: wrote a test
    
    Co-authored-by: jakobmertl <jakob.mertl@miragon.io>

--- apps/miragon-process-ide-cli/src/app/deployment/deployment.ts
+++ apps/miragon-process-ide-cli/src/app/deployment/deployment.ts
@@ -1,4 +1,4 @@
-import { DigiwfLib } from "@miragon-process-ide/digiwf-lib";
+import {checkIfSupportedType, DigiwfLib, FileDetails } from "@miragon-process-ide/digiwf-lib";
 import { getFile, getFiles } from "../shared/fs";
 import * as colors from "colors";
 
@@ -8,12 +8,7 @@ export class Deployment {
 
     public async deployArtifact(path: string, type: string, project: string | undefined, target: string): Promise<void> {
         const file = await getFile(path);
-        const artifact = await this.digiwfLib.deploy(target, {
-            "type": type,
-            "project": project ?? "",
-            "file": file
-        });
-        console.log(colors.green.bold("DEPLOYED ") + artifact.file.name + " to environment " + target);
+        return await this.deploy(file, type, project, target);
     }
 
     public async deployAllArtifacts(path: string, project: string | undefined, target: string): Promise<void> {
@@ -24,16 +19,24 @@ export class Deployment {
                 if (type === "json") {
                     path.includes("schema.json") ? type = "form" : type = "config";
                 }
-                const artifact = {
-                    "type": type,
-                    "project": project ?? "",
-                    "file": file
-                }
-                await this.digiwfLib.deploy(target, artifact);
-                console.log(colors.green.bold("DEPLOYED ") + artifact.file.name + " to environment " + target);
+                await this.deploy(file, type, project, target);
             } catch (err) {
-                console.log(colors.red.bold("FAILED ") + ` deploying ${file.name} with -> ${err}`);
+                console.log(colors.red.bold("FAILED ") + `deploying ${file.name} with -> ${err}`);
             }
         }
+    }
+
+
+    private async deploy(file: FileDetails, type: string, project: string | undefined, target: string): Promise<void> {
+        //blacklisting invalid artifact-types
+        if (!checkIfSupportedType(type)) {
+            return Promise.reject(`${type} is not supported for deployment`);
+        }
+        const artifact = await this.digiwfLib.deploy(target, {
+            "type": type,
+            "project": project ?? "",
+            "file": file
+        });
+        console.log(colors.green.bold("DEPLOYED ") + artifact.file.name + " to environment " + target);
     }
 }
