5c1bfb9e96a1fc51e9040a7ed8b01e672a725ba1
Lukas Mösle <37705369+lmoesle@users.noreply.github.com>
2022-11-22 22:03:05 +0100

    fix: use process-ide.json in the current directory (#31)

--- apps/process-ide-console/src/app/deployment/deployment.ts
+++ apps/process-ide-console/src/app/deployment/deployment.ts
@@ -1,14 +1,17 @@
 import * as vscode from "vscode";
-import * as colors from "colors";
-import { createDigiwfLib, DigiWFDeploymentPlugin, DigiwfDeploymentPluginRest, DigiwfLib} from "@miragon-process-ide/digiwf-lib";
-import { getDigiWfLib } from "../../main";
+import { Artifact, createDigiwfLib, DigiWFDeploymentPlugin, DigiwfDeploymentPluginRest, DigiwfLib} from "@miragon-process-ide/digiwf-lib";
 
-const fs = vscode.workspace.fs;
+const ws = vscode.workspace;
+const fs = ws.fs;
 
-export async function mapProcessConfigToDigiwfLib(path: vscode.Uri): Promise<DigiwfLib> {
-    const p = vscode.Uri.joinPath(path,"../../../resources/my-process-automation-project/process-ide.json");
-    const processIdeJson = (await fs.readFile(p)).toString();
-    const processIdeConfig = JSON.parse(processIdeJson);
+export async function initDigiwfLib(): Promise<DigiwfLib> {
+    const processIdeJSON = await fs.readFile(vscode.Uri.joinPath(vscode.Uri.file(ws.rootPath ?? ""), "process-ide.json"));
+
+    if (!processIdeJSON) {
+        return new DigiwfLib();
+    }
+
+    const processIdeConfig = JSON.parse(processIdeJSON.toString());
     const plugins: DigiWFDeploymentPlugin[] = [];
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
@@ -18,37 +21,41 @@ export async function mapProcessConfigToDigiwfLib(path: vscode.Uri): Promise<Dig
     return createDigiwfLib(processIdeConfig.projectVersion, processIdeConfig.name, processIdeConfig.workspace, plugins);
 }
 
-
-export async function deployArtifact(path: vscode.Uri, target: string) {
-    const digiwfLib = getDigiWfLib();
+export async function getArtifact(path: vscode.Uri): Promise<Artifact> {
     const content = (await fs.readFile(path)).toString();
     const file = path.fsPath.substring(path.fsPath.lastIndexOf('/')+1).split('.');
+    return {
+        "type": file[1],
+        "project": "test" ?? "",     //should be the actual project-name
+        "file": {
+            "content": content,
+            "extension": file[1],
+            "name": file[0]
+        }
+    };
+}
+
+export async function getArtifacts(path: vscode.Uri): Promise<Artifact[]> {
+    const artifacts: Artifact[] = [];
+    const files = await fs.readDirectory(path);
     try {
-        const artifact = await digiwfLib.deploy(target, {
-            "type": file[1],
-            "project": "test" ?? "",     //should be the actual project-name
-            "file": {
-                "content": content,
-                "extension": file[1],
-                "name": file[0]
+        for (const file of files) {
+            const filePath = vscode.Uri.file(path.fsPath + "/" + file[0]);
+            if(file[1] != 1) {
+                artifacts.push(...(await getArtifacts(filePath)));
+            } else {
+                //only form and bpmn
+                artifacts.push(await getArtifact(filePath));
             }
-        });
-        vscode.window.showInformationMessage(colors.green.bold("DEPLOYED ") + artifact.file.name + " to environment " + target);
-    } catch(err: any) {
-        console.log(colors.red.bold("FAILED ") + ` deploying ${file} with -> ${err}`);
-        vscode.window.showInformationMessage(err.msg);
+        }
+        return artifacts;
+    }
+    catch (err) {
+        console.log(err);
+        return Promise.reject();
     }
 }
 
-export async function getUriAndDeploy(path: vscode.Uri, target: string) {
-    const files = await fs.readDirectory(path);
-    for (const file of files) {
-        const filePath = vscode.Uri.file(path.fsPath + "/" + file[0]);
-        if(file[1] != 1) {
-            getUriAndDeploy(filePath, target);
-        } else {
-            //only form and bpmn
-            await deployArtifact(filePath, target);
-        }
-    }
+export function fileDeploymentSupported(artifact: Artifact): boolean {
+    return artifact.type === "form" || artifact.type === "bpmn" || artifact.type === "config" || artifact.type === "dmn";
 }
