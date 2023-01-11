9b0a52da676e9839242cd3bd18bb5e31bc853b47
jakobmertl <jakob.mertl@miragon.io>
2023-01-11 10:27:41 +0100

    test: deployment - cli

--- apps/miranum-cli/src/app/deployment/api.spec.ts
+++ apps/miranum-cli/src/app/deployment/api.spec.ts
@@ -5,7 +5,7 @@ const target = "local"
 
 describe("deploy all files", () => {
     const projectPath = "resources/my-process-automation-project"
-    it(`generate a bpmn project`, async () => {
+    it(`should work`, async () => {
         const program = deployAllFiles();
         program.parse(["node", appPath, "deploy", "--directory", projectPath, "--target", target]);
 
@@ -18,7 +18,7 @@ describe("deploy all files", () => {
 
 describe("deploy files", () => {
     const bpmn = {path: "resources/my-process-automation-project/my-process.bpmn", type: "bpmn"}
-    it(`generate a bpmn project`, async () => {
+    it(`should work`, async () => {
         const program = deployFileCommand();
         program.parse(["node", appPath, "deploy-file", "--file", bpmn.path, "--target", target, "--type", bpmn.type]);
 
@@ -26,5 +26,57 @@ describe("deploy files", () => {
         expect(program.opts().file).toBe(bpmn.path);
         expect(program.opts().target).toBe(target);
         expect(program.opts().type).toBe(bpmn.type);
+    });
+
+    it(`should not work, due to wrong type`, async () => {
+        const miranumJson = {path: "resources/my-process-automation-project/miranum.json", type: "json"}
+        const program = deployFileCommand();
+        program
+            .exitOverride()
+            .command("generate")
+            .action(() => {});
+        let errorMsg;
+        try {
+            program.parse(["node", appPath, "deploy-file", "--file", miranumJson.path, "--target", target, "--type", miranumJson.type]);
+        } catch (error) {
+            errorMsg = error;
+        }
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        expect(errorMsg.code).toBe("commander.invalidArgument");
+    });
+
+    it("should not work, due to unknown option", () => {
+        const program = deployFileCommand();
+        program
+            .exitOverride()
+            .command("generate")
+            .action(() => {});
+        let errorMsg;
+        try {
+            program.parse(["node", appPath, "deploy-file", "--file", bpmn.path, "--target", target, "--type", bpmn.type, "--randomArgument"]);
+        } catch (error) {
+            errorMsg = error;
+        }
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        expect(errorMsg.code).toBe("commander.unknownOption");
+    });
+
+    it(`should not work, due to missing argument`, async () => {
+        const program = deployFileCommand();
+        program
+            .exitOverride()
+            .command("generate")
+            .action(() => {});
+        let errorMsg;
+        try {
+            program.parse(["node", appPath, "deploy-file", "--file", bpmn.path, "--type", bpmn.type]);
+        } catch (error) {
+            errorMsg = error;
+        }
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        expect(errorMsg.code).toBe("commander.missingMandatoryOptionValue");
     });
 });
