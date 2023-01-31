9c55c8ed6be2666873e36b563416b7073986a5bd
Jakob Mertl <42836653+J-Mertl@users.noreply.github.com>
2023-01-31 16:17:54 +0100

    feat/docForTesting(#61)
    * doc: wrote doc for testing
    
    * refactor: watch-commands
    
    * refactor: implemented change request
    
    * refactor: reset and renamed project.json
    
    * refactor: development.md to observe command
    
    * chore: use custom webpack config for observe command too
    
    ---------
    
    Co-authored-by: jakobmertl <jakob.mertl@miragon.io>
    Co-authored-by: Lukas Mösle <lukas95m@yahoo.de>

--- apps/miranum-console-webview/src/app/components/GenerateProjectInput.tsx
+++ apps/miranum-console-webview/src/app/components/GenerateProjectInput.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import {useCallback, useEffect, useMemo, useState} from 'react';
-import {useVsMessage} from "./Hooks/Message";
+import * as React from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { useVsMessage } from "./Hooks/Message";
 import { Avatar, Box, Button, FormControl, TextField, Typography } from "@mui/material";
 import { CreateNewFolder } from "@mui/icons-material";
 import FileSelector from "./UI/FileSelector";
@@ -15,24 +15,24 @@ const GenerateProjectInput: React.FC<Props> = props => {
     const [name, setName] = useState<string>(props.name);
     const [path, setPath] = useState<string>(props.currentPath);
     const [pressed, setPressed] = useState<boolean>(false);
-    const [error, setError] = useState<string>("")
+    const [error, setError] = useState<string>("");
     const inputChange = useVsMessage("changedInput");
     const sendProjectMessage = useVsMessage("generateProject");
-    const digiwfLib = useMemo(() => {return new MiranumCore()}, []);
+    const digiwfLib = useMemo(() => {return new MiranumCore();}, []);
 
     useEffect(() => {
         setPath(props.currentPath);
     }, [props.currentPath]);
 
     const generate = useCallback(() => {
-        if(name !== "" && path !== "") {
+        if (name !== "" && path !== "") {
             digiwfLib.initProject(name)
                 .then((artifacts: Artifact[]) => {
                     sendProjectMessage({
                         name: name,
                         path: path,
-                        artifacts: artifacts
-                    })
+                        artifacts: artifacts,
+                    });
                 })
                 .then(() => setError(""))
                 .catch((err) => setError(err.message));
@@ -40,55 +40,55 @@ const GenerateProjectInput: React.FC<Props> = props => {
     }, [name, path, digiwfLib, sendProjectMessage]);
 
     return (
-            <FormControl
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}
-            >
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <CreateNewFolder />
-                </Avatar>
-                <Typography component="h1" variant="h5">
+        <FormControl
+            sx={{
+                marginTop: 8,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+            }}
+        >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                <CreateNewFolder />
+            </Avatar>
+            <Typography component="h1" variant="h5">
                     Generate Project
-                </Typography>
-                <Box component="form" noValidate sx={{ mt: 1 }}>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="name"
-                        label="Name"
-                        name="name"
-                        value={name}
-                        onChange={e => {
-                            setName(e.target.value);
-                            inputChange({name: e.target.value});
-                        }}
-                        autoFocus
-                        error={name === ''}
-                        helperText={(name === '' && pressed)? 'You have to insert a name!':' '}
-                    />
-                    <FileSelector
-                        path={path}
-                        setPath={ (p:string) => setPath(p)}
-                    />
-                    <Button
-                        onClick={() => {
-                            setPressed(!name || !path);
-                            generate();
-                        }}
-                        fullWidth
-                        variant="contained"
-                        color="secondary"
-                        sx={{ mt: 3, mb: 2 }}
-                    >generate project</Button>
-                </Box>
-                {error !== '' && <Typography variant="subtitle1" borderColor="red">{error}</Typography>}
-            </FormControl>
+            </Typography>
+            <Box component="form" noValidate sx={{ mt: 1 }}>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="name"
+                    label="Name"
+                    name="name"
+                    value={name}
+                    onChange={e => {
+                        setName(e.target.value);
+                        inputChange({ name: e.target.value });
+                    }}
+                    autoFocus
+                    error={name === ""}
+                    helperText={(name === "" && pressed) ? "You have to insert a name!" : " "}
+                />
+                <FileSelector
+                    path={path}
+                    setPath={ (p:string) => setPath(p)}
+                />
+                <Button
+                    onClick={() => {
+                        setPressed(!name || !path);
+                        generate();
+                    }}
+                    fullWidth
+                    variant="contained"
+                    color="secondary"
+                    sx={{ mt: 3, mb: 2 }}
+                >generate project</Button>
+            </Box>
+            {error !== "" && <Typography variant="subtitle1" borderColor="red">{error}</Typography>}
+        </FormControl>
     );
-}
+};
 
 export default GenerateProjectInput;
