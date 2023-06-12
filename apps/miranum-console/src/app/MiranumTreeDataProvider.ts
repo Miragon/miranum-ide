540874e1932db028d51b01d1900325cdb4521b16
peterhnm <peter.hnm@gmx.de>
2023-06-12 12:27:34 +0200

    feat: add TreeViewProvider

---
+++ apps/miranum-console/src/app/MiranumTreeDataProvider.ts
@@ -0,0 +1,128 @@
+import * as vscode from "vscode";
+import {
+    Command,
+    FileType,
+    ThemeIcon,
+    TreeDataProvider,
+    TreeItem,
+    TreeItemCollapsibleState,
+    Uri,
+    WorkspaceFolder,
+} from "vscode";
+
+export class MiranumTreeDataProvider implements TreeDataProvider<Artifact> {
+    private readonly workspace?: WorkspaceFolder;
+
+    constructor(workspace?: WorkspaceFolder) {
+        this.workspace = workspace;
+
+        vscode.commands.registerCommand("miranum.treeView.openFile", (path: Uri) => {
+            vscode.commands.executeCommand("vscode.open", path);
+        });
+    }
+
+    getChildren(element?: Artifact): Thenable<Artifact[]> {
+        if (!this.workspace) {
+            vscode.window.showInformationMessage("No workspace open!");
+            return Promise.resolve([]);
+        }
+
+        if (element) {
+            return Promise.resolve(
+                this.getArtifactsInProject(
+                    Uri.joinPath(this.workspace.uri, element.label),
+                ),
+            );
+        } else {
+            return Promise.resolve(
+                this.getArtifactsInProject(Uri.joinPath(this.workspace.uri)),
+            );
+        }
+    }
+
+    getTreeItem(element: Artifact): TreeItem | Thenable<TreeItem> {
+        return element;
+    }
+
+    private async getArtifactsInProject(path: Uri): Promise<Artifact[]> {
+        if (!this.workspace) {
+            throw Error("No workspace is open!");
+        }
+
+        const artifacts: Artifact[] = [];
+        let files: [string, FileType][] = await vscode.workspace.fs.readDirectory(path);
+
+        files = files.sort((a, b) => {
+            if (a[1] === b[1]) {
+                // sort filenames alphabetically
+                const filenameA = a[0].toLowerCase();
+                const filenameB = b[0].toLowerCase();
+                if (filenameA < filenameB) {
+                    return -1;
+                }
+                if (filenameA > filenameB) {
+                    return 1;
+                }
+            }
+            // sort directories for files
+            return b[1] - a[1];
+        });
+
+        for (const file of files) {
+            switch (file[1]) {
+                case FileType.Directory: {
+                    artifacts.push(
+                        new Artifact(file[0], TreeItemCollapsibleState.Collapsed),
+                    );
+                    break;
+                }
+                case FileType.File: {
+                    const fileUri = Uri.joinPath(path, file[0]);
+                    artifacts.push(
+                        new Artifact(
+                            file[0],
+                            TreeItemCollapsibleState.None,
+                            {
+                                arguments: [fileUri],
+                                command: "miranum.treeView.openFile",
+                                title: "Open",
+                            },
+                            fileUri,
+                        ),
+                    );
+                    break;
+                }
+            }
+        }
+
+        return artifacts;
+    }
+}
+
+class Artifact extends TreeItem {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    public readonly label;
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    public readonly collapsibleState: TreeItemCollapsibleState;
+
+    constructor(
+        label: string,
+        collapsibleState: TreeItemCollapsibleState,
+        command?: Command,
+        path?: Uri,
+    ) {
+        super(label, collapsibleState);
+
+        this.label = label;
+        this.collapsibleState = collapsibleState;
+        this.command = command;
+        this.resourceUri = path;
+
+        if (collapsibleState === TreeItemCollapsibleState.None) {
+            this.iconPath = ThemeIcon.File;
+        }
+    }
+}
--- apps/miranum-console/src/main.ts
+++ apps/miranum-console/src/main.ts
@@ -3,21 +3,34 @@ import { initMiranumCore } from "./app/shared/fs-helpers";
 import { registerDeploymentCommands } from "./app/deployment/commands";
 import { showErrorMessage } from "./app/shared/message";
 import { registerGenerateCommands } from "./app/generate/commands";
+import { MiranumTreeDataProvider } from "./app/MiranumTreeDataProvider";
 
-export async function activate(context : vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
     const miranumCore = await initMiranumCore();
 
     // generate
     const generateCommands = await registerGenerateCommands(context, miranumCore);
-    generateCommands.forEach(command => context.subscriptions.push(command));
+    generateCommands.forEach((command) => context.subscriptions.push(command));
 
     // deployment
     try {
-        const deploymentCommands = await registerDeploymentCommands(context, miranumCore);
-        deploymentCommands.forEach(command => context.subscriptions.push(command));
+        const deploymentCommands = await registerDeploymentCommands(
+            context,
+            miranumCore,
+        );
+        deploymentCommands.forEach((command) => context.subscriptions.push(command));
     } catch (err) {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         showErrorMessage(err.message);
     }
+
+    const workspace =
+        vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0
+            ? vscode.workspace.workspaceFolders[0]
+            : undefined;
+    vscode.window.registerTreeDataProvider(
+        "project-view",
+        new MiranumTreeDataProvider(workspace),
+    );
 }
