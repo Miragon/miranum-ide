b84f83ac5cedcef0d541ece0f2cc57aabae112bb
Lukas Mösle <lukas95m@yahoo.de>
2023-08-04 15:38:37 +0200

    feature: add constructor to DeploymentFailedException

--- spring-boot-apps/miranum-deployment/miranum-deployment-receiver-core/src/main/java/io/miragon/miranum/deploymentreceiver/application/DeploymentFailedException.java
+++ spring-boot-apps/miranum-deployment/miranum-deployment-receiver-core/src/main/java/io/miragon/miranum/deploymentreceiver/application/DeploymentFailedException.java
@@ -1,7 +1,14 @@
 package io.miragon.miranum.deploymentreceiver.application;
 
+import lombok.NoArgsConstructor;
+
 /**
  * Exception thrown when a deployment failed.
  */
+@NoArgsConstructor
 public class DeploymentFailedException extends RuntimeException {
+
+    public DeploymentFailedException(String message) {
+        super(message);
+    }
 }
