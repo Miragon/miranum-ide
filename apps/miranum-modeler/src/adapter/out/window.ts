33719074906c2ca5f0dc69eb207b1f543c05972d
peterhnm <peter.heinemann@miragon.io>
2024-06-20 11:46:09 +0200

    fix(jsonforms): add a global theme and resolve toggle text editor bug

--- apps/miranum-modeler/src/adapter/out/window.ts
+++ apps/miranum-modeler/src/adapter/out/window.ts
@@ -1,11 +1,4 @@
-import {
-    LogOutputChannel,
-    Tab,
-    TabInputText,
-    ViewColumn,
-    window,
-    workspace,
-} from "vscode";
+import { window } from "vscode";
 
 import {
     DisplayMessageOutPort,
@@ -13,7 +6,7 @@ import {
     OpenLoggingConsoleOutPort,
     TextEditorOutPort,
 } from "../../application/ports/out";
-import { getContext } from "@miranum-ide/vscode/miranum-vscode";
+import { VsCodeLogger, VsCodeTextEditor } from "@miranum-ide/vscode/miranum-vscode";
 
 export class VsCodeDisplayMessageAdapter implements DisplayMessageOutPort {
     info(message: string): void {
@@ -26,96 +19,29 @@ export class VsCodeDisplayMessageAdapter implements DisplayMessageOutPort {
 }
 
 export class VsCodeTextEditorAdapter implements TextEditorOutPort {
-    private isOpen = false;
-
-    private activeDocumentPath = "";
-
-    constructor() {
-        const changeTab = window.tabGroups.onDidChangeTabs((tabs) => {
-            // Event when user closes the tab with the document
-            tabs.closed.forEach((tab) => {
-                if (
-                    tab.input instanceof TabInputText &&
-                    tab.input.uri.path === this.activeDocumentPath
-                ) {
-                    this.isOpen = false;
-                }
-            });
-        });
-
-        getContext().subscriptions.push(changeTab);
-    }
+    private readonly textEditor = new VsCodeTextEditor();
 
     async toggle(documentPath: string): Promise<boolean> {
-        if (this.isOpen) {
-            this.isOpen = await this.close(documentPath);
-        } else {
-            this.isOpen = await this.open(documentPath);
-        }
-
-        if (this.isOpen) {
-            this.activeDocumentPath = documentPath;
-        } else {
-            this.activeDocumentPath = "";
-        }
-
-        return this.isOpen;
-    }
-
-    private async open(documentPath: string): Promise<boolean> {
-        try {
-            const textDocument = await workspace.openTextDocument(documentPath);
-            await window.showTextDocument(textDocument, ViewColumn.Beside);
-            return true;
-        } catch (error) {
-            return false;
-        }
-    }
-
-    private async close(documentPath: string): Promise<boolean> {
-        const tab = this.getTab(documentPath);
-
-        if (tab) {
-            return window.tabGroups.close(tab);
-        } else {
-            return false;
-        }
-    }
-
-    private getTab(documentPath: string): Tab | undefined {
-        for (const tabGroup of window.tabGroups.all) {
-            for (const tab of tabGroup.tabs) {
-                if (
-                    tab.input instanceof TabInputText &&
-                    tab.input.uri.path === documentPath
-                ) {
-                    return tab;
-                }
-            }
-        }
-        return undefined;
+        return this.textEditor.toggle(documentPath);
     }
 }
 
 export class VsCodeLoggerAdapter
     implements OpenLoggingConsoleOutPort, LogMessageOutPort
 {
-    private readonly prefix = "[MiranumIDE.Modeler] ";
+    private readonly id = "MiranumIDE.Modeler";
 
-    private readonly logger: LogOutputChannel = window.createOutputChannel(
-        "MiranumIDE.Modeler",
-        { log: true },
-    );
+    private readonly logger = new VsCodeLogger(this.id);
 
     open() {
-        this.logger.show(true);
+        this.logger.open();
     }
 
     info(message: string) {
-        this.logger.info(this.prefix + message);
+        this.logger.info(message);
     }
 
     error(error: Error) {
-        this.logger.error(this.prefix, error);
+        this.logger.error(error);
     }
 }
